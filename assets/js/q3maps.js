449910071f071d2fe31e0c6122decf7f982e8388
Leanne Teeter <lrteeter@gmail.com>
2025-05-13 23:31:34 -0400

    adding way more images. work in progress.

--- assets/js/q3maps.js
+++ assets/js/q3maps.js
@@ -78,7 +78,7 @@
         loadLightbox();
     };
 
-    function loadMaps(searchTerm = null, sortOrder = 'native', filters_include = [], filters_exclude =[]) {
+    async function loadMaps(searchTerm = null, sortOrder = 'native', filters_include = [], filters_exclude =[]) {
         var tableRows = [];
         for (let i = 0; i < mapsArray.length; i++) {
 
@@ -122,24 +122,21 @@
             tableRows += '<div class="row">';
 
             // image
-            tableRows += '<a href="images/q3maps/' + mapsArray[i].id + '.jpg" class="col-4 col-12-xsmall image-container">';
-            tableRows += '<img src="images/q3maps/' + mapsArray[i].id + '.jpg" height="384" width="512" alt="' + mapsArray[i].name + '" title="' + mapsArray[i].name + '" />';
-            for (let n = 1; n < 4; n++) {
-                let filename = 'images/q3maps/' + mapsArray[i].id + '-' + n + '.jpg';
-                fetch(filename, {
-                    method: 'HEAD',
-                }).then((res) => {
-                    if (res.ok) {
-                        tableRows += '<img src="' + filename + '" height="384" width="512" alt="' + mapsArray[i].name + '" title="' + mapsArray[i].name + '" />';
-                    } else {
-                        return;
-                    }
-                });
-            }
+            tableRows += '<a href="images/q3maps/' + mapsArray[i].id + '-0.jpg" class="col-4 col-8-small col-12-xsmall image-container">';
+            tableRows += '<img src="images/q3maps/' + mapsArray[i].id + '-0.jpg" height="384" width="512" alt="' + mapsArray[i].name + ' image 1" title="' + mapsArray[i].name + ' image 1" />';
             tableRows += '</a>';
 
+            tableRows += '<div class="col-2 col-4-small col-12-xsmall">';
+            for (let n = 1; n < 3; n++) {
+                let filename = 'images/q3maps/' + mapsArray[i].id + '-' + n + '.jpg';
+                tableRows += '<a href="' + filename + '" class="image-container">';
+                tableRows += '<img src="' + filename + '"  height="192" width="256" alt="' + mapsArray[i].name + ' image ' + Number(n+1) + '" title="' + mapsArray[i].name + ' image ' + Number(n+1) +'" />';
+                tableRows += '</a>';
+            }
+            tableRows += '</div>';
+
             // name and id
-            tableRows += '<div class="col-8 col-12-xsmall">';
+            tableRows += '<div class="col-4 col-12-small info-container">';
             tableRows += '<h3>' + mapsArray[i].name + '</h3>';
             tableRows += '<div>ID: ' + mapsArray[i].id + '</div>';
 
@@ -159,7 +156,6 @@
             }
 
             tableRows += '</div>';
-
             tableRows += '</div>';
         }
 
