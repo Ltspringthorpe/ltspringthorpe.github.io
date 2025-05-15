062c16036d6bce93f152f1d1027df39475283fda
Leanne Springthorpe <lrteeter@gmail.com>
2025-05-14 20:57:31 -0400

    first commit in letting users set favorites

--- assets/js/q3maps.js
+++ assets/js/q3maps.js
@@ -129,6 +129,7 @@
             // name and id
             tableRows += '<div class="col-8 col-12-xsmall">';
             tableRows += '<h3>' + mapsArray[i].name + '</h3>';
+            tableRows += '<div class="filter-items star-icon"><a onclick="toggleFavorite()" data-id="' + mapsArray[i].id + '"></a></div>';
             tableRows += '<div>ID: ' + mapsArray[i].id + '</div>';
 
             // keywords
@@ -171,7 +172,68 @@
         });
     };
 
+    function getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function handleModal() {
+        var cookieConsent = getCookie('cookieConsent');
+        if (!!cookieConsent) {
+            $('#cookieConsent').addClass('hidden');
+            $('body').removeClass('modal-open');
+        }
+    }
+
+    function getFavorites() {
+        var cookieConsent = getCookie('cookieConsent');
+        if (!!cookieConsent) {
+            let decodedCookie = decodeURIComponent(document.cookie);
+            let ca = decodedCookie.split(';');
+            for (let i = 0; i < ca.length; i++) {
+                let mapId = ca[i].trim('=');
+                if ($('.star-icon a[data-id="' + mapId + '"]').lenth) {
+                    $('.star-icon a[data-id="' + mapId + '"]').addClass('favorite');
+                }
+            }
+        }
+    }
+
+    // cookies accepted
+    $('#cookieConsent #yes').on('click', function() {
+        $('#cookieConsent').addClass('hidden');
+        $('body').removeClass('modal-open');
+
+        setCookie('cookieConsent', true, 3650);
+    });
+
+    // cookied denied
+    $('#cookieConsent #no, #cookieConsent .closer').on('click', function() {
+        $('#cookieConsent').addClass('hidden');
+        $('body').removeClass('modal-open');
+    });
+
     $(window).on('load', function() {
         init();
+        handleModal();
+        getFavorites();
     });
 })(jQuery);
