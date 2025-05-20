0f35550daaf7d5d79a2d2bea481a66464090d1a0
Leanne Springthorpe <lrteeter@gmail.com>
2025-05-20 14:32:23 -0400

    adding buttons for favorite show/hide/sort. not yet functional.

--- assets/js/q3maps.js
+++ assets/js/q3maps.js
@@ -10,7 +10,7 @@
         // set up listener for filter carets
         $('.caret').on('click', function() {
             $(this).toggleClass('closed');
-            $(this).closest('.filter-list').find('.filter-items').toggleClass('hidden');
+            $(this).closest('.filter-list').find('.filter-items, .favorites').toggleClass('hidden');
         });
 
         // set up listeners for filter buttons
