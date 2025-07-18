e005fca7af4670ec2821e3e396bde0dd66853f9f
Leanne Springthorpe <lrteeter@gmail.com>
2025-07-18 17:18:05 -0400

    bug in set cookie timeout

--- assets/js/q3maps.js
+++ assets/js/q3maps.js
@@ -276,6 +276,13 @@
         $('.star-icon a').on('click', toggleFavorite);
     }
 
+    function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
     $(window).on('load', function() {
         init();
     });
